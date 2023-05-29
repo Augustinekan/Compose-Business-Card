b2022b8f1120d9388081619f20607ee0a759951b
Augustinekan <augustinekanyuira0@gmail.com>
2023-05-29 12:55:25 +0300

    add and align footer tiles

--- app/src/main/java/com/example/composebusinesscard/MainActivity.kt
+++ app/src/main/java/com/example/composebusinesscard/MainActivity.kt
@@ -8,13 +8,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material.icons.rounded.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -48,13 +54,20 @@ fun ComposeBusinessCard(modifier: Modifier = Modifier){
     val screenHeight = configuration.screenHeightDp.dp
     val screenWidth = configuration.screenWidthDp.dp
 
-    Box(
-        modifier = modifier
-        .background(color = Color(0xff94aeb2))
-        .height(height = screenHeight)
-        .width(width = screenWidth), contentAlignment = Alignment.Center){
-         ImageProfileComposable()
-    }
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = modifier
+                .background(color = Color(0xff94aeb2))
+                .height(height = screenHeight)
+                .width(width = screenWidth),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(){}
+            ImageProfileComposable()
+            FooterSection()
+
+        }
+
 }
 
 @Composable
@@ -72,7 +85,9 @@ fun ImageProfileComposable(modifier: Modifier = Modifier){
 @Composable
 fun DeveloperImage(modifier: Modifier = Modifier){
     val imageResource = painterResource(id = R.drawable.passport_photo);
-    Image(painter = imageResource, contentDescription = "Passport Image", modifier = modifier.width(width = 140.dp).height(height = 140.dp)
+    Image(painter = imageResource, contentDescription = "Passport Image", modifier = modifier
+        .width(width = 120.dp)
+        .height(height = 120.dp)
        )
 }
 
@@ -84,4 +99,28 @@ fun DeveloperName(modifier: Modifier = Modifier){
 @Composable
 fun Tagline(){
     Text(text = stringResource(id = R.string.AndroidDev), color = Color(0xff1b443a), fontSize = 18.sp)
+}
+
+
+@Composable
+fun FooterSection(modifier: Modifier = Modifier){
+Column (
+    modifier = modifier.padding(16.dp)
+        ){
+    FooterTile(
+        icon = Icons.Rounded.Phone,
+        text = "+254 777 888 ***"
+    )
+    FooterTile(icon = Icons.Rounded.Share, text ="@AndroidDev" )
+    FooterTile(icon = Icons.Rounded.Email, text = "augustinekanyuira0@gmail.com")
+}
+}
+@Composable
+fun FooterTile(icon: ImageVector,text:String, modifier: Modifier = Modifier){
+    Row (
+        modifier = modifier.padding(vertical = 4.dp)
+            ){
+        Icon( imageVector = icon,  "phone", modifier = modifier.padding(horizontal = 12.dp),tint = Color(0xff1b443a))   // ok
+        Text(text = text)
+    }
 }
